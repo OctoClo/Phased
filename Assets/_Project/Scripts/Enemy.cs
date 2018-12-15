dbaac041844d592382cad251b32b931423e24c0f
Claudia <octoclo@zoho.com>
2018-12-15 18:44:24 +0100

    Enemy moving towards players

---
+++ Assets/_Project/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int speed;
+
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        if (speed == 0)
+        {
+            speed = Random.Range(5, 13);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0, 0, -1);
+        rigidBody.velocity = movement * speed;
+    }
+}
