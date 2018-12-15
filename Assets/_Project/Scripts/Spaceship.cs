691a902389d557d71fd8c17ba9250b3007779295
Claudia <octoclo@zoho.com>
2018-12-15 17:51:18 +0100

    First draft of spaceship movement and camera

---
+++ Assets/_Project/Scripts/Spaceship.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("L_XAxis_1");
+        float moveVertical = -Input.GetAxis("L_YAxis_1");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rigidBody.velocity = movement * speed;
+
+        rigidBody.rotation = Quaternion.Euler(rigidBody.velocity.z * tilt, 0, rigidBody.velocity.x * -tilt);
+    }
+}
