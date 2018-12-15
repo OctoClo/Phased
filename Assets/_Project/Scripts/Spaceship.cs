407126bb88bf903a211fe9c16dddf71140d333c5
foobar <foo@bar.net>
2018-12-15 22:11:38 +0100

    Target sphere proto (acting as aim target)

--- Assets/_Project/Scripts/Spaceship.cs
+++ Assets/_Project/Scripts/Spaceship.cs
@@ -6,9 +6,13 @@ public class Spaceship : MonoBehaviour
 {
     public float speed;
     public float tilt;
+
+    public float targetRadius = 2.0f;
+
     public int playerNumber;
 
     public PlayerInputManager inputManager;
+    public GameObject cursor;
 
     Rigidbody rigidBody;
 
@@ -28,5 +32,18 @@ public class Spaceship : MonoBehaviour
         rigidBody.velocity = movement * speed;
 
         rigidBody.rotation = Quaternion.Euler(rigidBody.velocity.z * tilt, 0, rigidBody.velocity.x * -tilt);
+
+        var target = inputManager.target[playerNumber];
+
+        var targetPosition = transform.position;
+        targetPosition.x += target.x * targetRadius;
+        targetPosition.z += target.y * targetRadius;
+
+        if ( target.magnitude != 0.0f )
+            cursor.transform.position = targetPosition;
+        
+        float angle = Mathf.Atan2(targetPosition.z - transform.position.z, targetPosition.x - transform.position.x) * Mathf.Rad2Deg;
+
+        cursor.transform.rotation = Quaternion.Euler(-90, 0.0f, angle * -1);
     }
 }
