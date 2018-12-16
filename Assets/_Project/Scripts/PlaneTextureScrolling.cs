9940f025e9bececbff850fd314cfe0f554644df1
foobar <foo@bar.net>
2018-12-16 16:34:55 +0100

    Plane scrolling script texture

---
+++ Assets/_Project/Scripts/PlaneTextureScrolling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneTextureScrolling : MonoBehaviour
+{ // Scroll main texture based on time
+
+    public float ScrollSpeed = 0.5f;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * ScrollSpeed;
+
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
