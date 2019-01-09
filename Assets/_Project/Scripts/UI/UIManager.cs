37a41accf043883cc2f9820354e79a0e58073edf
Claudia <octoclo@zoho.com>
2019-01-09 11:24:39 +0100

    Score displayed in UI

--- Assets/_Project/Scripts/UI/UIManager.cs
+++ Assets/_Project/Scripts/UI/UIManager.cs
@@ -5,35 +5,43 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public List<Spaceship> Spaceships = new List<Spaceship>();
-
     public LifeCounter lifeCounter;
+    public Text HUDLifeCounter;
 
+    public Text score;
+    public Text multiplicator;
+
+    public List<Spaceship> Spaceships = new List<Spaceship>();
     public List<Image> phaseZones = new List<Image>();
 
     public GameObject gameOverOverlay;
-    public GameObject hudOverlay;
-
-    public Text HUDLifeCounter;
+    public GameObject hudOverlay;    
     
     void Update()
     {
         if ( lifeCounter.LifeCount <= 0 )
         {
-            foreach (Spaceship spaceship in Spaceships)
-            {
-                spaceship.GetComponentInChildren<Renderer>().enabled = false;
-            }
-
-            hudOverlay.SetActive(false);
-            gameOverOverlay.SetActive(true);
+            GameOver();
         }
 
         HUDLifeCounter.text = "x" + lifeCounter.LifeCount;
+        score.text = GameScore.Score.ToString();
+        multiplicator.text = "x" + GameScore.Multiplicator.ToString();
 
         for (int i = 0; i < 2; i++)
         {
             phaseZones[i].transform.position = Spaceships[i].transform.position;
         }
+    }
+
+    void GameOver()
+    {
+        foreach (Spaceship spaceship in Spaceships)
+        {
+            spaceship.GetComponentInChildren<Renderer>().enabled = false;
+        }
+
+        hudOverlay.SetActive(false);
+        gameOverOverlay.SetActive(true);
     }
 }
