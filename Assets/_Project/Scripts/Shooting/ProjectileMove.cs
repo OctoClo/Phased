14f11218da82f1b565a25f2c8af2ada8c03fb6b1
Tarti <anais.riff@orange.fr>
2019-01-13 21:07:12 +0100

    Projectile FX

---
+++ Assets/_Project/Scripts/Shooting/ProjectileMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+
+    void Start(){
+
+    }
+
+    void Update(){
+        if (speed != 0) { 
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }else{
+            Debug.Log("No Speed");
+        }
+     }
+}
