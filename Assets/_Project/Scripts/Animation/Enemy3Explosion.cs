11c3becc8c18e21a0553240afef8079f4d74dd0e
Tarti <anais.riff@orange.fr>
2019-01-13 17:33:11 +0100

    Vignette Vaisseaubdeath

---
+++ Assets/_Project/Scripts/Animation/Enemy3Explosion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3Explosion : MonoBehaviour { 
+
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    private void Start()
+    {
+        Explode();
+    }
+
+    public void Explode()
+    {
+        foreach (Transform t in transform)
+        {
+
+            var rb = t.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(UnityEngine.Random.Range(minForce, maxForce), transform.position, radius);
+            }
+        }
+    }
+}
