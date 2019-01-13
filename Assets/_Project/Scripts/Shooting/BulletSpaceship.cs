3bdc2c2cf526aca4e7bc500a0f849f3bcda50525
Clément <clement.pilate@gmail.com>
2019-01-13 16:34:08 +0100

    Add Impact FX to Bullet collision

--- Assets/_Project/Scripts/Shooting/BulletSpaceship.cs
+++ Assets/_Project/Scripts/Shooting/BulletSpaceship.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class BulletSpaceship : Bullet
 {
     public int Damage = 1;
-
+    public GameObject Impact_FX;
+    
     void OnTriggerEnter(Collider other)
     {
         EnemySphere enemy = other.GetComponent<EnemySphere>();
 
         if (enemy)
         {
+
+            GameObject impactFXGO = Instantiate(Impact_FX);
+            impactFXGO.transform.position = transform.position;
+            impactFXGO.transform.rotation = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up);
+
+            ParticleSystem impactFX = impactFXGO.GetComponent<ParticleSystem>();
+            impactFX.Play();
+
             enemy.TakeDamage(Damage);
             Destroy(gameObject);
         }
---
+++ Assets/_Project/Scripts/VFX/ParticleSystemAutoDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemAutoDestroy : MonoBehaviour
+{
+    private ParticleSystem ps;
+
+
+    public void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+
+        Debug.Log("ParticleSystem alive");
+    }
+
+    public void Update()
+    {
+        if (ps)
+        {
+            if (!ps.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
