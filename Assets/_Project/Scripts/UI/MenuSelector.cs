2d2b2d697d82aced961059e42e40e316124e38cf
Claudia <octoclo@zoho.com>
2019-02-07 20:43:23 +0100

    Fixes for mouse + gamepad menu controls

--- Assets/_Project/Scripts/UI/MenuSelector.cs
+++ Assets/_Project/Scripts/UI/MenuSelector.cs
@@ -1,124 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.EventSystems;
-// using UnityEngine.Experimental.Input;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Linq;
 using Rewired;
 
 public class MenuSelector : MonoBehaviour
 {
-	[SerializeField]
-    private Button[] buttons;
-
-    [SerializeField]
-    private float blockInputTime = 0.2f;
-
-    [SerializeField]
-    private bool horizontalMenu = false;
-
-    private int lastSelectedButton;
-	private int selectedButton;
-    
-    private float timeBetweenButtons = 0.2f;
+    public Selectable FirstSelectable;
+    public bool VerticalLayout = true;
 
     EventSystem eventSystem;
-    //Keyboard keyboard;
 
-    bool waiting = true;
-
-    private void Start()
+    void Awake()
     {
-        Debug.Log("Start menu selector");
         eventSystem = EventSystem.current;
-        buttons[0].Select();
-        eventSystem.SetSelectedGameObject(buttons[selectedButton].gameObject);
     }
 
-    private void OnEnable()
+    void OnEnable() 
     {
-        if (!eventSystem)
-            eventSystem = EventSystem.current;
-
-        selectedButton = 0;
-        lastSelectedButton = -1;
-
-        StartCoroutine(WaitBeforeInput(blockInputTime));
+        StartCoroutine(SelectFirstButtonLater());
     }
 
-    private void OnDisable()
+    void Update()
     {
-        eventSystem.SetSelectedGameObject(null);
-    }
-
-    IEnumerator WaitBeforeInput(float time)
-    {
-        waiting = true;
-        yield return new WaitForSecondsRealtime(time);
-        waiting = false;
-    }
-
-    private void Update()
-	{
-        if (!waiting)
+        if (eventSystem.currentSelectedGameObject == null && ReInput.players.AllPlayers.Any(player => player.GetAxis(VerticalLayout ? "UIVertical" : "UIHorizontal") != 0f))
         {
-            //keyboard = InputSystem.GetDevice<Keyboard>();
-
-            // if (lastSelectedButton != selectedButton)
-            // {
-            //     buttons[selectedButton].Select();
-            //     eventSystem.SetSelectedGameObject(buttons[selectedButton].gameObject);
-            //     lastSelectedButton = selectedButton;
-            // }
-
-            // if (Gamepad.all.Any(x => x.buttonSouth.wasReleasedThisFrame)
-            //     || (keyboard != null && keyboard.enterKey.wasReleasedThisFrame))
-            // {
-            //     AkSoundEngine.PostEvent("menu_ok", gameObject);
-            //     buttons[selectedButton].onClick.Invoke();
-            // }
-
-            // if (NextSelection())
-            // {
-            //     AkSoundEngine.PostEvent("menu_navigation", gameObject);
-            //     selectedButton = (selectedButton + 1) % buttons.Length;
-            //     StartCoroutine(WaitBeforeInput(timeBetweenButtons));
-            // }
-            // else if (PreviousSelection())
-            // {
-            //     AkSoundEngine.PostEvent("menu_navigation", gameObject);
-            //     selectedButton--;
-            //     if (selectedButton < 0)
-            //     {
-            //         selectedButton = buttons.Length - 1;
-            //     }
-            //     StartCoroutine(WaitBeforeInput(timeBetweenButtons));
-            // }
+            StartCoroutine(SelectFirstButtonLater());
         }
-	}
+    }
 
-    // private bool NextSelection()
-    // {
-    //     if (horizontalMenu)
-    //     {
-    //         return (Gamepad.all.Any(x => x.leftStick.ReadValue().x > 0.75F) || (keyboard != null && keyboard.dKey.wasReleasedThisFrame));
-    //     }
-    //     else
-    //     {
-    //         return (Gamepad.all.Any(x => x.leftStick.ReadValue().y < -0.75F) || (keyboard != null && keyboard.sKey.wasReleasedThisFrame));
-    //     }
-    // }
-
-    // private bool PreviousSelection()
-    // {
-    //     if (horizontalMenu)
-    //     {
-    //         return (Gamepad.all.Any(x => x.leftStick.ReadValue().x < -0.75F) || (keyboard != null && keyboard.aKey.wasReleasedThisFrame));
-    //     }
-    //     else
-    //     {
-    //         return (Gamepad.all.Any(x => x.leftStick.ReadValue().y > 0.75F) || (keyboard != null && keyboard.wKey.wasReleasedThisFrame));
-    //     }
-    // }    
+    private IEnumerator SelectFirstButtonLater()
+    {
+        yield return null;
+        eventSystem.SetSelectedGameObject(FirstSelectable.gameObject);
+    }
 }
